2a14be695a0cf1657898fefc625a3e3bde7e9501
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-09-07 14:06:49 -0700

    initial files

---
+++ data/animals.js
@@ -0,0 +1,10 @@
+
+
+var animals = [
+	{name: 'Wombat', image: 'images/wombat.jpg', extra: 'Size: Small<br>Color: Brown<br>Food: Veggies'},
+	{name: 'Turtle', image: 'images/turtle.jpg', extra: 'Size: Small<br>Color: Green<br>Food: Peaches'},
+	{name: 'Eagle', image: 'images/eagle.jpg', extra: 'Size: Medium<br>Color: Black &amp; White<br>Food: Fish'},
+	{name: 'Bull', image: 'images/bull.jpg', extra: 'Size: Large<br>Color: Black<br>Food: Grass'},
+]
+
+module.exports(animals);
---
+++ webserver.js
@@ -0,0 +1,13 @@
+
+//require the animals.js file from the data folder, store it in a variable
+
+//require express
+
+//make the server
+
+//add the static folder and point it at the html folder
+
+//make an endpoint to get data for all animals
+	//return the animals data
+
+//make the server listen for connections on port 3000
