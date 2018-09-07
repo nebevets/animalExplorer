c38ff6bbdf9d6afc0c3c9236eed0fad50615f343
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-09-07 14:30:03 -0700

    added full main.js with test.js

--- html/main.js
+++ html/main.js
@@ -25,6 +25,9 @@ function renderOneAnimal( animal ){
 	const container = $("<div>",{
 		'class': 'animalContainer'
 	});
+	container.click(()=>{
+		displayOneAnimal(animal);
+	})
 	const name = $("<div>", {
 		'class': 'name',
 		text: animal.name
@@ -37,6 +40,28 @@ function renderOneAnimal( animal ){
 	});
 	container.append(name, avatar);
 	$("#animalList").append(container);
+}
+/*
+		<div class="name">Wombat</div>
+		<div class="avatar large"></div>
+		<div class="details">Size: small<br>Color: brown<br>Food: veggies</div>
+		*/
+function displayOneAnimal( animal ){
+	const name = $("<div>",{
+		'class': 'name',
+		text: animal.name
+	});
+	const avatar = $("<div>",{
+		'class': 'avatar large',
+		css: {
+			backgroundImage: `url(${animal.image})`
+		},		
+	});
+	const details = $("<div>",{
+		'class': 'details',
+		html: animal.extra
+	});
+	$("#animalDetails").empty().append(name, avatar, details);
 }
 
 
