6e50477baaf5e8473d10a5aa605b9d5bad274d4c
nebevets <steveben@exmsft.com>
2018-09-25 09:43:15 -0700

    implemented php version

--- html/data.php
+++ html/data.php
@@ -4,7 +4,7 @@ $animalsJSON = file_get_contents('test.js');
 $animals = json_decode($animalsJSON, true); // true makes an associative array instead of a php object
 
 function sortFunction($a, $b) {
-    return $a["name"] <=> $b["name"]; //spaceship opertor
+    return $a["name"] <=> $b["name"]; //spaceship opertor 1<=>1 returns 0 (equal), 3<=>4 returns -1 (less), 4<=>3 returns 1 (greater)
 }
 
 usort($animals, "sortFunction");
